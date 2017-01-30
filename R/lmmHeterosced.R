8ed22a973016c8f6e09527a9c1d5d08b95dbea50
Bolstad <geir.bolstad@nina.no>
2017-01-30 08:51:36 +0100

    Basic function that works

---
+++ R/lmmHeterosced.R
@@ -0,0 +1,96 @@
+#' Linear mixed model with heteroscedastisity
+#'
+#' \code{lmmHeterosced} Linear mixed model with the log error variance as linear function of data.
+#'
+#' @param formula model formula lmer style
+#' @param data a data frame with the data
+#' @param heterosced_formula an optional heteroscadisity formula
+#'
+#' @return \code{lmmHeterosced} a list with elements:
+#' Fixef (fixed effects estaimtes),
+#' Heterosced (parameters of the heteroscedasticity function, a linear function of the log residual variance),
+#' Ranef_log_Var (random effect variances on log),
+#' Ranef (random effect),
+#' fit (nlminb output),
+#' obj (MakeADFun ouptut),
+#' optTime (Optimization time)
+#'
+#' @author Geir H. Bolstad
+#'
+#' @examples
+#'
+#' @importFrom TMB MakeADFun sdreport
+#' @importFrom lme4 lFormula lmerControl
+#' @importFrom Matrix Matrix t
+#' @useDynLib lmmHeterosced
+#'
+#' @export
+
+
+lmmHeterosced <- function(formula, data, heterosced_formula = ~ 1){
+
+  mf <- lFormula(formula, data)
+
+  Y <- as.matrix(mf$fr[1])
+  X <- Matrix(mf$X, sparse = T)
+  Z <- t(Matrix(mf$reTrms$Zt))
+  n_random <- ncol(mf$reTrms$flist)
+  n_u <- apply(mf$reTrms$flist, 2, function(x) length(unique(x)))
+  u_start <- c(0, cumsum(n_u))
+  u_end <- cumsum(n_u)
+
+  # The model matrix log residual variance
+    Q <- model.matrix(heterosced_formula, data = data)
+
+
+  #----------------
+  # Starting values
+  #----------------
+  param <- list(b = matrix(0, ncol = 1, nrow = ncol(X)),
+                u = matrix(0, ncol = 1, nrow = ncol(Z)),
+                b_ln_R = matrix(0, ncol = 1, nrow = ncol(Q)),
+                ln_G = rep(0, n_random)
+                )
+  random <- c("b", "u") # the fixed effects are added to get REML estimates
+
+  #------------
+  # Data to TMB
+  #------------
+  dt <- list(Y = Y, X = X, Z = Z,
+             n_random = n_random,
+             u_start = u_start,
+             u_end = u_end,
+             Q = Q)
+
+
+  #--------------
+  # Model fitting
+  #--------------
+  obj <- MakeADFun(dt, param, DLL = "lmmHeterosced", random=random, silent = TRUE)
+  optTime <- system.time(
+    fit <- with(obj, nlminb(start=par, objective=fn, gradient=gr))
+  )
+
+  #-------
+  # Return
+  #-------
+
+  results <- summary(sdreport(obj))
+
+  Fixef <- matrix(results[which(rownames(results)=="b"),], ncol = 2,
+                  dimnames=list(dimnames(X)[[2]], c("Estimate", "Std. Error")))
+  Heterosced <- matrix(results[which(rownames(results)=="b_ln_R"),], ncol = 2,
+                       dimnames=list(dimnames(Q)[[2]], c("Estimate", "Std. Error")))
+  Ranef_log_Var <-  matrix(results[which(rownames(results)=="ln_G"),], ncol = 2,
+                           dimnames=list(colnames(mf$reTrms$flist), c("Estimate", "Std. Error")))
+  Ranef <- matrix(results[which(rownames(results)=="u"),], ncol = 2,
+                  dimnames=list(dimnames(Z)[[2]], c("Estimate", "Std. Error")))
+
+  Residuals <- matrix(results[which(rownames(results)=="residuals"),], ncol = 2,
+                  dimnames=list(NULL, c("Estimate", "Std. Error")))
+
+  list(Fixef=Fixef, Heterosced=Heterosced, Ranef_log_Var=Ranef_log_Var, Ranef=Ranef, Residuals = Residuals,
+       fit = fit, obj = obj, optTime = optTime)
+
+}
+
---
+++ src/lmmHeterosced.cpp
@@ -0,0 +1,59 @@
+#include <TMB.hpp>
+
+template<class Type>
+Type objective_function<Type>::operator() ()
+{
+
+  // Data
+  DATA_MATRIX(Y);        // The response varaible
+  DATA_SPARSE_MATRIX(X); // The fixed effects design matrix
+  DATA_SPARSE_MATRIX(Z); // The random effects design matrix
+  DATA_INTEGER(n_random);
+  DATA_FACTOR(u_start);
+  DATA_FACTOR(u_end);
+  DATA_MATRIX(Q);        // Design matrix for the residual variance
+
+
+  // Parameters
+  PARAMETER_MATRIX(b);        // The fixed effects
+  PARAMETER_MATRIX(u);        // The random effects
+  PARAMETER_MATRIX(b_ln_R);   // The linear parameters the log residual variance
+  PARAMETER_VECTOR(ln_G);     // log variances of the random effects
+
+
+  int i, j, k;
+  Type nll=0;
+  int n = Y.rows();
+
+  // Mixed model equation
+  vector<Type> residuals = Y - X*b - Z*u;
+
+
+  //----------
+  // Residuals
+  //----------
+  // log residual variance
+  vector<Type> ln_R = Q*b_ln_R;
+
+  for(i=0;i<n;i++){
+    nll -= dnorm(residuals(i), Type(0.0), exp(Type(0.5)*ln_R(i)), true);
+  }
+
+
+  //---------------
+  // Random effects
+  //---------------
+
+  for(k=0; k<n_random; k++){
+    for(i=u_start(k);i<u_end(k);i++){
+      nll -= dnorm(u(i), Type(0.0), exp(Type(0.5)*ln_G(k)), true);
+    }
+  }
+
+  ADREPORT(residuals);
+
+  return nll;
+}
+
+
+
